9a724fb46d5a05098d495071ffb7ac039cf33b7d
David <david@davidmorgadegil.com>
2024-07-12 12:38:49 +0200

    starting project

---
+++ astro-blog-backend/src/api/post/content-types/post/lifecycles.js
@@ -0,0 +1,19 @@
+const readingTime = require("reading-time");
+
+module.exports = {
+  async beforeCreate(event) {
+    console.log("########## BEFORE CREATE ##########");
+    if (event.params.data.content) {
+      event.params.data.readingTime =
+        readingTime(event.params.data.content)?.text || null;
+    }
+  },
+
+  async beforeUpdate(event) {
+    console.log("########## BEFORE UPDATE ##########");
+    if (event.params.data.content) {
+      event.params.data.readingTime =
+        readingTime(event.params.data.content)?.text || null;
+    }
+  },
+};
