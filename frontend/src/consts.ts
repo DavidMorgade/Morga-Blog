a3e8bee048b2245bc15dca57209fac90bcb71d82
David <davidmorgadegil@gmail.com>
2024-07-18 18:55:52 +0200

    created single blog page fully working with markdown

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -2,4 +2,9 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'David Morgade - Blog';
+
 export const SITE_DESCRIPTION = 'Blog personal de Morga';
+
+export const UPLOADS_URL = 'http://localhost:1337';
+
+export const LONG_API_URL = 'http://localhost:1337/api/posts?populate[featured_image][fields][0]=name&populate[featured_image][fields][1]=width&populate[featured_image][fields][2]=height&populate[featured_image][fields][3]=url&populate[author][populate][bioImage][fields][0]=name&populate[author][populate][bioImage][fields][1]=width&populate[author][populate][bioImage][fields][2]=height&populate[author][populate][bioImage][fields][3]=url&populate[categories][populate]=true'
--- frontend/src/helpers/fetchPosts.ts
+++ frontend/src/helpers/fetchPosts.ts
@@ -3,7 +3,7 @@ import { type PostRequest } from "../types/postRequest";
 
 export async function fetchPosts() {
 
-    const res = await fetch('http://localhost:1337/api/posts?populate[featured_image][fields][0]=name&populate[featured_image][fields][1]=width&populate[featured_image][fields][2]=height&populate[featured_image][fields][3]=url&populate[author][populate][bioImage][fields][0]=name&populate[author][populate][bioImage][fields][1]=width&populate[author][populate][bioImage][fields][2]=height&populate[author][populate][bioImage][fields][3]=url&populate[categories][populate]=true');
+    const res = await fetch('http://localhost:1337/api/posts?populate[featured_image][fields][0]=name&populate[featured_image][fields][1]=width&populate[featured_image][fields][2]=height&populate[featured_image][fields][3]=url&populate[author][populate][image][fields][0]=name&populate[author][populate][image][fields][1]=width&populate[author][populate][image][fields][2]=height&populate[author][populate][image][fields][3]=url&populate[categories][populate]=true');
     const data: PostRequest = await res.json();
 
 
