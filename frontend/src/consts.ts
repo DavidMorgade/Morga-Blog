bcb37d239fd60f876701bb95e363ac3ed85a1c89
David <davidmorgadegil@gmail.com>
2024-08-22 17:43:37 +0200

    finishing about and newsletter

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -16,7 +16,7 @@ const LONG_API_URL = `${UPLOADS_URL}/api/posts?populate[featured_image][fields][
 const LONG_API_URL_PRODUCTION = `${PRODUCTION_URL}/api/posts?populate[featured_image][fields][0]=name&populate[featured_image][fields][1]=width&populate[featured_image][fields][2]=height&populate[featured_image][fields][3]=url&populate[author][populate][image][fields][0]=name&populate[author][populate][image][fields][1]=width&populate[author][populate][image][fields][2]=height&populate[author][populate][image][fields][3]=url&populate[categories][populate]=true`
 
 
-export const URL_LONG = isProd ? LONG_API_URL_PRODUCTION : LONG_API_URL;
-export const URL = isProd ? PRODUCTION_URL : UPLOADS_URL;
+export const URL_LONG = LONG_API_URL_PRODUCTION;
+export const URL = PRODUCTION_URL;
 
 export const URL_PORTFOLIO = 'https://www.codigomorga.es/portfolio';
