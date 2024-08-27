6113b554050ba32a5bc021c156bf0fe5eb32d9be
David <david@davidmorgadegil.com>
2024-08-27 09:33:16 +0200

    testing routes for the server

--- backend/src/api/post/content-types/post/lifecycles.js
+++ backend/src/api/post/content-types/post/lifecycles.js
@@ -1,9 +1,11 @@
 const { exec } = require('child_process');
 
+const route = '/home/proyectos/backend/deploy-script.sh';
+
 module.exports = {
   async afterUpdate(event) {
     // Ejecuta el script bash al actualizar una entrada
-    exec('/home/backend/deploy-script.sh', (error, stdout, stderr) => {
+    exec(route, (error, stdout, stderr) => {
       console.log('Ejecutando script al actualizar una entrada')
       if (error) {
         console.error(`Error al ejecutar el script: ${error}`);
@@ -18,7 +20,7 @@ module.exports = {
   },
   async afterCreate(event) {
     // Ejecuta el script bash al crear una entrada
-    exec('/home/backend/deploy-script.sh', (error, stdout, stderr) => {
+    exec(route, (error, stdout, stderr) => {
       console.log('Ejecutando script al crear una entrada')
       if (error) {
         console.error(`Error al ejecutar el script: ${error}`);
@@ -33,7 +35,7 @@ module.exports = {
   },
   async afterDelete(event) {
     // Ejecuta el script bash al eliminar una entrada
-    exec('/home/backend/deploy-script.sh', (error, stdout, stderr) => {
+    exec(route, (error, stdout, stderr) => {
       console.log('Ejecutando script al eliminar una entrada')
       if (error) {
         console.error(`Error al ejecutar el script: ${error}`);
